ce4c21bdbea2b50629159d2dbbf6347c1c21d908
hiiike <58051188+hiiike@users.noreply.github.com>
2020-11-15 21:38:12 +0200

    lab4.5(2)

--- lab4.5/Source.cpp
+++ lab4.5/Source.cpp
@@ -4,30 +4,42 @@
 using namespace std;
 int main()
 {
-	double x, y, R;
+	double x, y, R, x0, y0, x1, y1;
+
 	srand((unsigned)time(NULL));
 	for (int i = 0; i < 10; i++)
 	{
 		cout << "R = "; cin >> R;
 		cout << "x = "; cin >> x;
 		cout << "y = "; cin >> y;
-		if (y <= (sqrt(R * R - (x + R) * (x + R))) || y <= (-sqrt(R * R - (x - R) * (x - R))))
+
+		x0 = R;
+		y0 = 0;
+		x1 = -R;
+		y1 = 0;
+
+		if (((x > 0 && x < R) && (y < 0 && y > -R) && ((x - x0) * (x - x0) + (-y0) * (y - y0) <= R * R)) ||
+			(((x < 0 && x > -R) && (y > 0 && y < R)) && ((x - x1) * (x - x1) + (y - y1) * (y - y1))))
 			cout << "yes" << endl;
 		else
 			cout << "no" << endl;
-	}	
+	}
 	cout << endl << fixed;
 
-		for (int i = 0; i < 10; i++)
-		{
-			x = -R + rand() * (2 * R) / RAND_MAX;
-			y = -R + rand() * (2 * R) / RAND_MAX;
-			if (y <= (sqrt(R * R - (x + R) * (x + R))) || y <= (-sqrt(R * R - (x - R) * (x - R))))
-				cout << setw(8) << setprecision(4) << x << " "
-				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
-			else
-				cout << setw(8) << setprecision(4) << x << " "
-				<< setw(8) << setprecision(4) << y << " " << "no" << endl;
-		}
+	for (int i = 0; i < 10; i++)
+	{
+		x = R * 2 * rand() / RAND_MAX - R;
+		y = R * 2 * rand() / RAND_MAX - R;
+
+		if (((x > 0 && x < R) && (y < 0 && y > -R) && ((x - x0) * (x - x0) + (-y0) * (y - y0) <= R * R)) ||
+			(((x < 0 && x > -R) && (y > 0 && y < R)) && ((x - x1) * (x - x1) + (y - y1) * (y - y1))))
+
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+		else
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+	}
+	system("pause");
 	return 0;
-}
+}
